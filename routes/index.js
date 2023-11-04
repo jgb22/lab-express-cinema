307428671a0b8ff386c96ca04bf8966a31609833
jgb22 <eutuxisfet@gmail.com>
2023-11-04 01:22:44 +0100

    Done Final

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('./models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+
+//Get movies page// 
+
+router.get('/movies', (req, res, next) => {
+    console.log(movie);
+    Movie.find()
+        .then((movies) => {
+            res.render("movies", { movies })
+})
+    .catch((err) => {
+        console.log(err)
+    })
+})
+router.get("/movies/:id", (req,res,next)=>{
+    Movie.findById(req.params.id)
+    .then((movie)=>{
+        console.log(movie)
+        res.render('movie-detail',movie)
+    })
+    .catch((error) => {
+        console.log(error);
+      });
+
+})
+
 module.exports = router;
+
